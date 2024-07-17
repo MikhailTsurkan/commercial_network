9e40484aeafbb02fa2ebf678619e197c7f635599
Tsurkan <mikh.tsurkan@gmail.com>
2024-07-17 12:47:10 +0300

    cleanup code

--- users/managers.py
+++ users/managers.py
@@ -3,6 +3,9 @@ from django.db.models import Manager
 
 
 class HashedPasswordManager(Manager):
+    """
+    Менеджер модели User, позволяющий хранить пароли в зашифрованном виде
+    """
     def create(self, email, password, **kwargs):
         user = super().create(email=email, password=make_password(password), **kwargs)
         return user
