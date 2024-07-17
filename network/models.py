98bd2b32dc618dd8190582783f14ae77b3629634
Tsurkan <mikh.tsurkan@gmail.com>
2024-07-17 21:36:37 +0300

    refactor contacts model

--- network/models.py
+++ network/models.py
@@ -2,15 +2,39 @@ from django.db import models
 
 
 class Contact(models.Model):
-    email = models.EmailField(unique=True)
-    country = models.CharField(max_length=128)
-    city = models.CharField(max_length=128)
-    street = models.CharField(max_length=128)
-    building_number = models.CharField(max_length=16)
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email address",
+        help_text="Email address for contacting"
+    )
+    country = models.CharField(
+        max_length=128,
+        verbose_name="Country name",
+        help_text="Country of company location"
+    )
+    city = models.CharField(
+        max_length=128,
+        verbose_name="City name",
+        help_text="City of company location"
+    )
+    street = models.CharField(
+        max_length=128,
+        verbose_name="Street name",
+        help_text="Street where company is located"
+    )
+    building_number = models.CharField(
+        max_length=16,
+        verbose_name="Building number",
+        help_text="Building number in street where company is located"
+    )
 
     class Meta:
         verbose_name = 'Контакт'
         verbose_name_plural = 'Контакты'
+        help_text = 'Список контактов компаний'
 
     def __str__(self):
         return f'{self.email} - {self.country}, {self.city}, {self.street}, {self.building_number}'
+
+
+
