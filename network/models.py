6412d362fbbf6237164f82f270a19bdbced657bc
Tsurkan <mikh.tsurkan@gmail.com>
2024-07-17 22:06:17 +0300

    add product model

--- network/models.py
+++ network/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Contact(models.Model):
+    """
+    Модель контакта, содержащая контактные данные
+    """
     supplier = models.ForeignKey(
         to="Supplier",
         on_delete=models.CASCADE,
@@ -37,9 +40,45 @@ class Contact(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Контакт'
-        verbose_name_plural = 'Контакты'
-        help_text = 'Список контактов компаний'
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+        help_text = 'Contact details'
 
     def __str__(self):
         return f'{self.email} - {self.country}, {self.city}, {self.street}, {self.building_number}'
+
+
+class Product(models.Model):
+    """
+    Модель продукта, описывающая продукт, его модель и дату выхода на рынок
+    """
+    supplier = models.ManyToManyField(
+        to="Supplier",
+        related_name="products",
+        verbose_name="Supplier",
+        help_text="Supplier of product"
+    ),
+
+    name = models.CharField(
+        max_length=128,
+        verbose_name="Product name",
+        help_text="Name of the product"
+    ),
+    model = models.CharField(
+        max_length=128,
+        verbose_name="Product model",
+        help_text="Model of the product"
+    ),
+    release_date = models.DateField(
+        verbose_name="Release date",
+        help_text="Date when product was released on market"
+    )
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        help_text = 'Product details'
+
+    def __str__(self):
+        return f'{self.name} - {self.model}'
+
