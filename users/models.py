32db70061e34ae2a8b680356b1acfa46d86a8a4c
Tsurkan <mikh.tsurkan@gmail.com>
2024-07-17 11:59:30 +0300

    add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Модель пользователя, используется для аутентификации
+    """
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    username = None
+    email = models.EmailField(
+        verbose_name='почта',
+        help_text="user's email",
+        unique=True,
+    )
+
+    def __str__(self):
+        return getattr(self, self.USERNAME_FIELD)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
