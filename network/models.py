9362c00df676d9568ed56fe1e8d67a32d13c7f42
Tsurkan <mikh.tsurkan@gmail.com>
2024-07-17 21:22:07 +0300

    add contacts model

--- network/models.py
+++ network/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Contact(models.Model):
+    email = models.EmailField(unique=True)
+    country = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    street = models.CharField(max_length=128)
+    building_number = models.CharField(max_length=16)
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
+    def __str__(self):
+        return f'{self.email} - {self.country}, {self.city}, {self.street}, {self.building_number}'
